e19eeb989f6da0f61b8f2f8ab5b7dde08cbda1be
Yoichi Hirai <i@yoichihirai.com>
2016-07-01 22:20:22 +0200

    sketch: thinking how to model words and bytes.  hitting a Coq anomaly.

--- sketch.v
+++ sketch.v
@@ -353,6 +353,8 @@ Record variable_env :=
   ; venv_balance : address -> word
   ; venv_caller : address
   ; venv_value_sent : word
+  (* TODO: add the sequence of executed instructions.
+     would be useful for calculating the gas *)
   }.
 
 (* [update_balance adr v original] is similar to [original] except
@@ -996,3 +998,173 @@ Section Example1Continue.
 End Example1Continue.
 
 End AbstractExamples.
+
+
+Module ConcreteWord <: Word.
+
+  Require Import ZArith.
+
+  Require BinNums.
+  Require Cyclic.ZModulo.ZModulo.
+
+
+  Module WLEN <: Cyclic.ZModulo.ZModulo.PositiveNotOne.
+    Import BinNums.
+
+    Definition p : positive := xO (xO (xO (xO (xO (xO (xO (xO xH))))))).
+    Lemma not_one : p <> 1%positive.
+      unfold p.
+      congruence.
+    Qed.
+  End WLEN.
+
+  Module BLEN <: Cyclic.ZModulo.ZModulo.PositiveNotOne.
+    Import BinNums.
+
+    Definition p : positive := xO (xO (xO xH)).
+    Lemma not_one : p <> 1%positive.
+      unfold p.
+      congruence.
+    Qed.
+  End BLEN.
+
+  Module ALEN <: Cyclic.ZModulo.ZModulo.PositiveNotOne.
+    Import BinNums.
+
+    Definition p : positive := 160.
+    Lemma not_one : p <> 1%positive.
+      unfold p.
+      congruence.
+    Qed.
+  End ALEN.
+
+  Module W := Cyclic.ZModulo.ZModulo.ZModuloCyclicType WLEN.
+
+  Module B := Cyclic.ZModulo.ZModulo.ZModuloCyclicType BLEN.
+
+  Module A := Cyclic.ZModulo.ZModulo.ZModuloCyclicType ALEN.
+
+  Definition word := W.t.
+
+  Print W.ops.
+
+  Require Import Cyclic.Abstract.CyclicAxioms.
+
+  Definition word_eq (a : W.t) (b : W.t) :=
+    match ZnZ.compare a b with Eq => true | _ => false end.
+
+  Definition word_add (a b : W.t) :=
+    ZnZ.add a b.
+
+  Definition word_sub := ZnZ.sub.
+
+  Definition word_one := ZnZ.one.
+
+  Definition word_zero := ZnZ.zero.
+
+  Definition word_iszero := ZnZ.eq0.
+
+  Definition word_smaller a b :=
+    match ZnZ.compare a b with Lt => true | _ => false end.
+
+  Definition word_of_N := Z.of_N.
+
+  Definition N_of_word w := Z.to_N (ZnZ.to_Z w).
+
+  Open Scope N_scope.
+  Lemma N_of_word_of_N :
+    forall (n : N), n < 10000 -> N_of_word (word_of_N n) = n.
+  Proof.
+    intros n nsmall.
+    unfold N_of_word.
+    unfold word_of_N.
+    unfold ZnZ.to_Z.
+    simpl.
+    unfold ZModulo.to_Z.
+    unfold ZModulo.wB.
+    unfold WLEN.p.
+    unfold DoubleType.base.
+    rewrite Z.mod_small.
+    { rewrite N2Z.id.
+      reflexivity. }
+    split.
+    {
+      admit.
+    }
+    {
+      admit.
+    }
+  Admitted.
+
+
+  Definition raw_byte := B.t.
+  Inductive byte' :=
+  | ByteRaw : raw_byte -> byte'
+  | ByteOfWord : word -> nat -> byte'
+  .
+  Definition byte := byte'.
+
+  Definition address := A.t.
+
+  Definition address_of_word (w : word) : address := w.
+
+  Definition word_nth_byte (w : word) (n : nat) : byte :=
+    ByteOfWord w n.
+
+  Axiom word_of_bytes : list byte -> word.
+
+  Open Scope list_scope.
+
+  Require Import Coq.Lists.List.
+  Import ListNotations.
+  Lemma words_of_nth_bytes :
+    forall w b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17 b18 b19 b20 b21 b22 b23 b24 b25 b26 b27 b28 b29 b30 b31,
+    b0 = word_nth_byte w 0 ->
+    b1 = word_nth_byte w 1 ->
+    b2 = word_nth_byte w 2 ->
+    b3 = word_nth_byte w 3 ->
+    b4 = word_nth_byte w 4 ->
+    b5 = word_nth_byte w 5 ->
+    b6 = word_nth_byte w 6 ->
+    b7 = word_nth_byte w 7 ->
+    b8 = word_nth_byte w 8 ->
+    b9 = word_nth_byte w 9 ->
+    b10 = word_nth_byte w 10 ->
+    b11 = word_nth_byte w 11 ->
+    b12 = word_nth_byte w 12 ->
+    b13 = word_nth_byte w 13 ->
+    b14 = word_nth_byte w 14 ->
+    b15 = word_nth_byte w 15 ->
+    b16 = word_nth_byte w 16 ->
+    b17 = word_nth_byte w 17 ->
+    b18 = word_nth_byte w 18 ->
+    b19 = word_nth_byte w 19 ->
+    b20 = word_nth_byte w 20 ->
+    b21 = word_nth_byte w 21 ->
+    b22 = word_nth_byte w 22 ->
+    b23 = word_nth_byte w 23 ->
+    b24 = word_nth_byte w 24 ->
+    b25 = word_nth_byte w 25 ->
+    b26 = word_nth_byte w 26 ->
+    b27 = word_nth_byte w 27 ->
+    b28 = word_nth_byte w 28 ->
+    b29 = word_nth_byte w 29 ->
+    b30 = word_nth_byte w 30 ->
+    b31 = word_nth_byte w 31 ->
+    word_of_bytes
+    [b0; b1; b2; b3; b4; b5; b6; b7; b8; b9; b10; b11; b12; b13; b14; b15; b16;
+     b17; b18; b19; b20; b21; b22; b23; b24; b25; b26; b27; b28; b29; b30; b31] =
+    w.
+  Admitted.
+
+  Axiom event : Set.
+
+  Axiom memory_state : Set.
+  Axiom empty_memory : memory_state.
+  Axiom cut_memory : word -> word -> memory_state -> list byte.
+  Axiom cut_memory_zero_nil :
+    forall start m, cut_memory start word_zero m = nil.
+
+End ConcreteWord.
+
+(* Anomaly: Uncaught exception Not_found. Please report. *)
